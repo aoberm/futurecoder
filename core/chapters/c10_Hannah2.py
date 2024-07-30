dc93e78b1b9d1b09c64134fade90de1f5e555b9d
HannahKn <134328604+HannahKn123@users.noreply.github.com>
2024-07-30 16:54:15 +0200

    welcome

--- core/chapters/c10_Hannah2.py
+++ core/chapters/c10_Hannah2.py
@@ -21,17 +21,18 @@ This is a very simple Python program. Copy the coder an press “Run” to see w
         def program(self):
             print("hello world")
 
-    class WelcomeToPython(VerbatimStep):
+    class HelloWorld2(VerbatimStep):
         """
 Python is one of the most popular programming languages — it's been used to write millions of computer programs.
-
-This is a very simple Python program. Press “Run” to see what it does.
+This is a very simple Python program. Copy the coder an press “Run” to see what it does.
 
     __copyable__
     __program_indented__
         """
+
         def program(self):
             print("Welcome to Python")
+
 
     class TrueOrFalse(VerbatimStep):
         """
