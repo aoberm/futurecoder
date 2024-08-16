fd5613f523be7e52c54ad7d7e9313c79d35ee4b4
HannahKn <134328604+HannahKn123@users.noreply.github.com>
2024-08-16 12:49:55 +0200

    welcome

--- core/chapters/c11_Pandas.py
+++ core/chapters/c11_Pandas.py
@@ -8,8 +8,8 @@ from core.exercises import assert_equal, generate_string
 from core.text import ExerciseStep, Page, VerbatimStep, Disallowed, MessageStep
 
 
-class WorkingWithPandas(Page):
-    title = "Working with Pandas"
+class ExploreDataset(Page):
+    title = "Explore a Dataset"
 
     class importPandas(VerbatimStep):
         """
@@ -116,8 +116,6 @@ class WorkingWithPandas(Page):
     Now that you know the structure, you are interested in the names of the individual columns.
     To do this, display the names of the columns. What role do the columns play in this data set?
 
-    Write the correct solution instead of the question marks.
-
         __copyable__
         import pandas as pd
         import pyodide_http
@@ -166,8 +164,6 @@ class WorkingWithPandas(Page):
      Before we continue with the analysis, we want to get a summary of the data set to deepen our understanding.
      To do this, enter data.info() in the next code block and study the output.
 
-    Write the correct solution instead of the question marks.
-
         __copyable__
         import pandas as pd
         import pyodide_http
@@ -215,7 +211,104 @@ class WorkingWithPandas(Page):
 
     final_text = """
     Good job!
-    In the next lesson, we'll learn how to manipulate numbers in Python.
+    In the next lesson, we'll learn different dimensions of data quality.
+"""
+
+
+
+
+
+
+class DataQuality(Page):
+    title = "Working with Pandas"
+
+    class Introduction(VerbatimStep):
+        """
+    Data quality plays a decisive role in the analysis of large amounts of data.
+    In the following, various dimensions of data quality will be examined.
+        """
+
+    class MissingVal(VerbatimStep):
+        """
+    First of all, we want to test for missing data. Missing entries are marked with NULL in databases.
+    Output the percentage of missing data in columns or rows by cleverly linking the .isnull() and .sum() functions.
+
+        __copyable__
+        import pandas as pd
+        import pyodide_http
+
+        pyodide_http.patch_all()  # Notwendig damit Download geht
+        data = pd.read_csv('https://raw.githubusercontent.com/aoberm/futurecoder/master/Datasets/cereals.csv')
+
+        print(round(100*data.isnull().sum()/len(data)),2)
+
+        """
+
+        requirements = "hints"
+
+        hints = """ test """
+
+        def program(self):
+            import pandas as pd
+            import pyodide_http
+
+            pyodide_http.patch_all()  # Notwendig damit Download geht
+            data = pd.read_csv('https://raw.githubusercontent.com/aoberm/futurecoder/master/Datasets/cereals.csv')
+
+            print(round(100 * data.isnull().sum() / len(data)), 2)
+
+        program_in_text = False
+
+
+    class RedundantData(VerbatimStep):
+        """
+    Redundant data is often caused by data records being saved multiple times. Remove all duplicates to restore order.
+    Display the number of duplicates by using the number of data records before and after removing the duplicates.
+
+
+        __copyable__
+        import pandas as pd
+        import pyodide_http
+
+        pyodide_http.patch_all()  # Notwendig damit Download geht
+        data = pd.read_csv('https://raw.githubusercontent.com/aoberm/futurecoder/master/Datasets/cereals.csv')
+
+        print(round(100 * data.isnull().sum() / len(data)), 2)
+
+        number_of_rows = len(data)
+        data.drop_duplicates(subset=None, inplace=True)
+        number_of_rows_new = len(data)
+
+        number_of_duplicates = number_of_rows - number_of_rows_new
+        print(number_of_duplicates)
+        """
+
+        requirements = "hints"
+
+        hints = """ test """
+
+        def program(self):
+            import pandas as pd
+            import pyodide_http
+
+            pyodide_http.patch_all()  # Notwendig damit Download geht
+            data = pd.read_csv('https://raw.githubusercontent.com/aoberm/futurecoder/master/Datasets/cereals.csv')
+
+            print(round(100 * data.isnull().sum() / len(data)), 2)
+
+            number_of_rows = len(data)
+            data.drop_duplicates(subset=None, inplace=True)
+            number_of_rows_new = len(data)
+
+            number_of_duplicates = number_of_rows - number_of_rows_new
+            print(number_of_duplicates)
+
+        program_in_text = False
+
+
+    final_text = """
+    Good job!
+    In the next lesson, we'll learn to work with the data.
 """
 
 
