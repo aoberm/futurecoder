22445b4cb0bd6b6955b203260d4f9de82cba2d0a
JuliavonBredow <julia.von-bredow@uni-ulm.de>
2024-08-17 08:50:11 +0200

    Test stuff

--- core/chapters/c05_Julia_Test.py
+++ core/chapters/c05_Julia_Test.py
@@ -1,26 +1,24 @@
 # flake8: NOQA E501
 import ast
-import re
+from textwrap import dedent
+from typing import List
+import random
 
-from astcheck import is_ast_like
-from core.text import MessageStep, Page, Step, VerbatimStep
-from core import translation as t
+from core.exercises import assert_equal, generate_string
+from core.text import ExerciseStep, Page, VerbatimStep, Disallowed, MessageStep
 
-'''class TestClass(Page):
+class Playground(Page):
     title = "This is a test"
 
     class SimpleCalculator(VerbatimStep):
         """
-        You can use Python to do calculations. Let's do some simple calculations.
+    You can use Python to do calculations. Let's do some simple calculations. Copy the code and run it.
 
-        __program_intended__
-
-        Copy the code and run it.
+    __program_intended__       
         """
-
         program = "print(4 + 8)"
 
     final_text = """
 Good job!
-    """'''
+    """
 
