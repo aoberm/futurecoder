026ed12d96c97db5d1e5433fe4e2ebf3457e3bfc
JuliavonBredow <julia.von-bredow@uni-ulm.de>
2024-08-17 09:27:39 +0200

    Trying stuff

---
+++ core/chapters/c05_Playground.py
@@ -0,0 +1,37 @@
+# flake8: NOQA E501
+import ast
+from textwrap import dedent
+from typing import List
+import random
+
+from core.exercises import assert_equal, generate_string
+from core.text import ExerciseStep, Page, VerbatimStep, Disallowed, MessageStep
+
+
+class Playground(Page):
+    title = "This is a test"
+    
+    class ndarray(VerbatimStep):
+        """
+    Eines der zentralen Merkmal von NumPY ist sein N-dinesionales Array-Objekt oder "ndarray", ein schneller, flexibler Container für große Datenmengen in Python.
+    Importiere zuerst NumPy und dann erzeuge ein kleines Array:
+    
+        __copyable__
+        import numpy as np
+        data = np.array([[1.5, -0.1, 3], [0, -3, 6.5]])
+    
+        """
+        
+        requirements = "hints"
+
+        hints = """ Am einfachsten erzeugst du ein Array mit der *array*-Funktion. """
+
+        def program(self):
+            import numpy as np
+            data = np.array([[1.5, -0.1, 3], [0, -3, 6.5]])
+
+        program_in_text = False
+
+    final_text = """
+    Good job!
+"""
