5c0bb1d7d527e52bf6f027f9db1a0c1e1e3b3c3a
HannahKn <134328604+HannahKn123@users.noreply.github.com>
2024-09-02 12:30:49 +0200

    welcome

--- core/chapters/c10_numpy_in_python.py
+++ core/chapters/c10_numpy_in_python.py
@@ -640,14 +640,15 @@ class LoopingOverNestedLists(Page):
 
         program_in_text = False
 
-        class Quiz1(VerbatimStep):
-        """
-        Can you guess the correct output for the following code snippet? Run the code.
+    class Quiz1(VerbatimStep):
 
-            __copyable__
-            import numpy as np
-            array_1d = np.arange(10, 16)
-            print(array_1d)
+        """
+    Can you guess the correct output for the following code snippet? Run the code.
+
+        __copyable__
+        import numpy as np
+        array_1d = np.arange(10, 16)
+        print(array_1d)
 
         """
         requirements = "hints"
