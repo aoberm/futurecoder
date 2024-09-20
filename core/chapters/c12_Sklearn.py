bb043996a6a5acce803bb0c7c37fdcf610779cff
JuliavonBredow <julia.von-bredow@uni-ulm.de>
2024-09-20 09:12:02 +0200

    fixing typing errors

--- core/chapters/c12_Sklearn.py
+++ core/chapters/c12_Sklearn.py
@@ -488,7 +488,7 @@ class PracticeSklearn(Page):
         y = data['Price (Thousands of dollars)']
 
         # Splitting the data into training and testing sets
-        X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=?, random_state=42)
+        X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
         
         #Training a LinearRegression model
         model = ?
@@ -550,7 +550,7 @@ class PracticeSklearn(Page):
         y = data['Price (Thousands of dollars)']
 
         # Splitting the data into training and testing sets
-        X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=42, random_state=42)
+        X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
         
         #Training a LinearRegression model
         model = LinearRegression()
@@ -622,7 +622,7 @@ class PracticeSklearn(Page):
         y = data['Price (Thousands of dollars)']
 
         # Splitting the data into training and testing sets
-        X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=42, random_state=42)
+        X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=0.2, random_state=42)
         
         #Training a LinearRegression model
         model = LinearRegression()
